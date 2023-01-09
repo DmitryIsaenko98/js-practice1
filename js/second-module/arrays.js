7f97d6927d8d327d192a84ea41c1205290f15a91
DmitryIsaenko98 <disaenko78@gmail.com>
2023-01-09 20:40:48 +0200

    second module arrays

---
+++ js/second-module/arrays.js
@@ -0,0 +1,38 @@
+//Find the biggest number in array
+
+// const numbers = [12, 44, 56, 1, 39, 11, 5, 77, 32];
+// // console.log(numbers);
+// let biggestNumber = [0];
+
+// for (const number of numbers) {
+//   //   console.log(number);
+
+//   if (number > biggestNumber) {
+//     biggestNumber = number;
+//   }
+// }
+// console.log(biggestNumber);
+
+// const names = ['Mango', 'Poly', 'Ajax', 'Viktor', 'Leli'];
+// const string = names.join(', ');
+// console.log(string);
+
+//Change Register
+
+// const string = 'jAVAsCRIPT';
+// const letters = string.split('');
+// let changedString = '';
+
+// console.log(letters);
+
+// for (const letter of letters) {
+//   console.log(letter);
+
+//   changedString += letter === letter.toLowerCase() ? letter.toUpperCase() : letter.toLowerCase();
+// }
+// console.log(`changedString :`, changedString);
+
+//Join Title
+// const title = '10 Benefits of Java Script';
+// const slug = title.toLocaleLowerCase().split(' ').join('-');
+// console.log(slug);
